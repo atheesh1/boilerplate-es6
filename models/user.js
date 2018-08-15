93ccdc1ccd6abf1c595e591e13bfd0978faee7d8
logan <you@example.com>
2018-08-15 21:59:12 +0530

    removed mongoose version

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ let Schema = mongoose.Schema
 const UserSchema = new Schema({
     name: String,
     mailId: String,
-})
+}, { versionKey: false })
 
 let UserModel = mongoose.model('user', UserSchema)
 
